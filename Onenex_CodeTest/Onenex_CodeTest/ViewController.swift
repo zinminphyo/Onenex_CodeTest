d85209752c52bfe07d00859b37fc75cba6dbd5c5
Min <zinminphyo@neversitup.com>
2023-05-31 16:26:59 +0630

    Project setup.

---
+++ Onenex_CodeTest/Onenex_CodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Onenex_CodeTest
+//
+//  Created by Zin Min Phyo on 31/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
